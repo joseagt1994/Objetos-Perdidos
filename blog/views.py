87e7d2aa0b0c74d2840845b4eeb9094b6765e83d
joseagt1994 <josegiron10100@hotmail.com>
2017-08-16 23:46:34 -0600

    Agregar imagenes a una publicacion
    Metodo que sirve para gestionar las imagenes de una publicacion al
    momento de realizar una publicacion nueva de objeto perdido.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def agregarImagenes(request,cod_publicacion):
+    pub = Publicacion.objects.get(codigo=cod_publicacion)
+    
+    if request.method == 'POST':
+        print('HOLA!')
+        formulario = formImagen(request.POST,request.FILES)
+        print('Es:...')
+        print(formulario.is_valid())
+        if formulario.is_valid():
+            print('Funciona!')
+            imagen = formulario.save(commit=False)
+            print('Link....')
+            imagen.ruta = request.FILES['link']
+            imagen.publicacion = pub
+            imagen.save()
+    formulario = formImagen()
+    imagenes = Imagen.objects.raw('SELECT * FROM blog_imagen WHERE publicacion_id = %s',[cod_publicacion])
+    print('Imagenes...')
+    print(imagenes)
+    return render_to_response('imagenes.html',{'formulario':formulario,'imagenes':imagenes},context_instance=RequestContext(request))
