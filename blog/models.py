d1da8f7dea4e9120776986b355a1e450c4def22b
Jherson7 <jhersonaos7@gmail.com>
2017-08-17 00:30:40 -0500

    Update models.py
    Se agrego el modelo de publicar objeto perdido

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    
+    codigo = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=30)
+    fecha = models.DateField()
+    longitud = models.FloatField()
+    latitud = models.FloatField()
+    descripcion = models.TextField(blank=False, default='')
+    estado = models.BooleanField()
+    autor = models.ForeignKey(User)
+    
+    def __str__(self):
+        return self.titulo
+        
