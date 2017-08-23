f06b794c37acac46bf957de25d7d9dfce136e804
joseagt1994 <josegiron10100@hotmail.com>
2017-08-22 22:56:13 -0600

    Update Modelo Imagen

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Imagen(models.Model):
+    
+    codigo = models.AutoField(primary_key=True)
+    link = models.ImageField(upload_to='imagenes')
+    descripcion = models.TextField()
+    publicacion = models.ForeignKey(Publicacion)
+    
+    def __str__(self):
+        return self.link
