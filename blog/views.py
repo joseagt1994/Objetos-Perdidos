48f8cde9948b3e462f9919327925267655c56b49
tejediego <diegotejeda93@gmail.com>
2017-10-23 21:42:26 -0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def getUsuario_codigo(cod_usuario):
+    usuario = Usuario.objects.filter(codigo=cod_usuario).first()
+    if usuario != None:
+        return True
+    else:
+        return False
+        
+def getPublicaciones():
+    publicaciones = Publicacion.objects.raw('SELECT * FROM objetos_imagen GROUP BY publicacion_id')
+    print(len(list(publicaciones)))
+    print('Tager se quedo sin bateria jajaja')
+    return 1
